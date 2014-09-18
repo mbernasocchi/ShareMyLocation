769f3ad8aed0fa97223288dec5d6f08dd0675773
Marco Bernasocchi <marco@opengis.ch>
2014-09-18 18:17:56 +0200

    added restart service on sync frequancy change

--- app/src/main/java/ch/opengis/sharemylocation/ShareActivity.java
+++ app/src/main/java/ch/opengis/sharemylocation/ShareActivity.java
@@ -5,8 +5,10 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.Menu;
@@ -18,58 +20,41 @@ import android.widget.Switch;
 public class ShareActivity extends Activity {
 
     static final String TAG = "ShareMyLocation";
+    Intent GPSintent;
     LocationManager locationManager;
-
-
-    // called by onclick in the xml
-    public void toggle_sharing(View view) {
-        boolean on = ((Switch) view).isChecked();
-
-        Intent i = new Intent(this, GPSService.class);
-        if (on) {
-            // Get Location Manager and check for GPS location services
-            if(!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-                // disable the switch, so if the use cancels the dialog we have consistent state
-                ((Switch) view).setChecked(false);
-
-                // Build the alert dialog
-                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setTitle(R.string.location_services_inactive);
-                builder.setMessage(R.string.enable_gps);
-                builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        // Show location settings when the user acknowledges the alert dialog
-                        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                        startActivity(intent);
-                    }
-                });
-                builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                // Show location settings when the user acknowledges the alert dialog
-                            }});
-
-                Dialog alertDialog = builder.create();
-                alertDialog.setCanceledOnTouchOutside(false);
-                alertDialog.show();
-            }
-            else{
-                startService(i);
-            }
-        } else {
-            stopService(i);
-        }
-    }
-
+    SharedPreferences prefs;
+    SharedPreferences.OnSharedPreferenceChangeListener prefListener;
+    Switch sharing_toggle;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share);
+
+        GPSintent = new Intent(this, GPSService.class);
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        Switch sharing_toggle = (Switch) findViewById(R.id.sharing_toggle);
+        sharing_toggle = (Switch) findViewById(R.id.sharing_toggle);
         sharing_toggle.setChecked(GPSService.IS_RUNNING);
         Log.d(TAG, "GPS service running: " + GPSService.IS_RUNNING);
+
+
+        prefListener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+                    public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
+                        if (key.equals(getString(R.string.sync_frequency))) {
+                            Log.d(TAG, "Restarting sharing service due to share frequency change");
+                            reset_sharing();
+                        }
+                    }
+                };
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        prefs.registerOnSharedPreferenceChangeListener(prefListener);
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        prefs.unregisterOnSharedPreferenceChangeListener(prefListener);
     }
 
 
@@ -92,5 +77,56 @@ public class ShareActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    // called by onclick in the xml
+    public void toggle_sharing(View view) {
+        if (sharing_toggle.isChecked()) {
+            start_sharing();
+        } else {
+            stop_sharing();
+        }
+    }
+
+    private void reset_sharing(){
+        if (GPSService.IS_RUNNING){
+            stopService(GPSintent);
+            startService(GPSintent);
+        }
+    }
+
+    private void stop_sharing() {
+        stopService(GPSintent);
+    }
+    private void start_sharing() {
+        // Get Location Manager and check for GPS location services
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            startService(GPSintent);
+        }
+        else {
+            // disable the switch, so if the use cancels the dialog we have consistent state
+            sharing_toggle.setChecked(false);
+
+            // Build the alert dialog
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle(R.string.location_services_inactive);
+            builder.setMessage(R.string.enable_gps);
+            builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    // Show location settings when the user acknowledges the alert dialog
+                    Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    startActivity(intent);
+                }
+            });
+            builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    // Show location settings when the user acknowledges the alert dialog
+                }
+            });
+
+            Dialog alertDialog = builder.create();
+            alertDialog.setCanceledOnTouchOutside(false);
+            alertDialog.show();
+        }
     }
 }
